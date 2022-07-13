0335faf7674427d8491d5402189a4405d139ec6c
jarvis1801 <chrisgithub2682@gmail.com>
2022-07-13 20:32:12 +0800

    first commit

---
+++ src/main/kotlin/com/jarvis/Application.kt
@@ -0,0 +1,13 @@
+package com.jarvis
+
+import io.ktor.server.application.*
+import com.jarvis.plugins.*
+
+fun main(args: Array<String>): Unit =
+    io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // application.conf references the main function. This annotation prevents the IDE from marking it as unused.
+fun Application.module() {
+    configureRouting()
+    configureSerialization()
+}
